367078741a37f9cb1b6cf047a90796698cac3a9f
Rafet <professionalraf@gmail.com>
2022-08-19 13:25:11 +0400

    Quick view section responsive completed

--- assets/js/quickWiew.js
+++ assets/js/quickWiew.js
@@ -5,6 +5,7 @@ let closeIcon=document.querySelector(".quickview-section .close .close-icon")
 let qvArea=document.querySelector(".view-area")
 let qvImg=document.querySelector(".quickview-img")
 let qvTitle=document.querySelector(".product-title")
+console.log(qvTitle);
 let qvBrand=document.querySelector(".qv-brand")
 let qvId=document.querySelector(".qv-id")
 let qvPrice=document.querySelector(".qv-price")
@@ -15,17 +16,16 @@ quickBtns.forEach((btn)=>{
         let img=imgs[imgs.length-1]
         let id=this.parentElement.parentElement.parentElement.getAttribute("data-id")
         let name=this.parentElement.parentElement.querySelector(".name").innerText
+        console.log(name);
         let price=this.parentElement.parentElement.querySelector(".price").innerText
         let brand=this.parentElement.parentElement.parentElement.getAttribute("brand")
-        console.log(img,id,name,price,brand);
-        console.log(brand);
         qvImg.src=`../assets/images/${img}`
         qvTitle.innerText=name;
         qvBrand.innerText=brand
         qvId.innerText=`Product ${id}`
         qvPrice.innerText=`${price} AZN`
         qvTaxi.innerText=`${Math.floor(price/100)*100} AZN`
-        
+        document.body.style.overflow="hidden"
         quickSection.style.display="flex"
     })
 })
@@ -38,5 +38,6 @@ quickSection.addEventListener("click",function(e){
     e.stopPropagation()
     if (e.target.offsetParent != qvArea) {
         quickSection.style.display="none"
+        document.body.style.overflow="auto"
     }
 })
