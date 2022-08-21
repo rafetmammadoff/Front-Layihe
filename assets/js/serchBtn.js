376157863dec5cb789fecb8e3b795e842dc91fe4
Rafet <professionalraf@gmail.com>
2022-08-21 14:43:30 +0400

    version 1.0.1 Update

--- assets/js/serchBtn.js
+++ assets/js/serchBtn.js
@@ -1,20 +1,25 @@
-let searchBtn=document.querySelector(".search-btn")
-let searchDDMenu=document.querySelector(".search-dropdown-menu")
-searchBtn.addEventListener("click",function(e){
-    searchDDMenu.classList.toggle("open-menu")
-    let children=Array.from(searchDDMenu.children)
-    let total=0;
-    children.forEach((child)=>{
-        total+=child.clientHeight
+let searchBtns=document.querySelectorAll(".search-btn")
+
+searchBtns.forEach((searchBtn)=>{
+    searchBtn.addEventListener("click",function(e){
+        console.log();
+        let searchDDMenu=e.target.parentElement.querySelector(".search-dropdown-menu")
+        searchDDMenu.classList.toggle("open-menu")
+        let children=Array.from(searchDDMenu.children)
+        let total=0;
+        children.forEach((child)=>{
+            total+=child.clientHeight
+        })
+        if (searchDDMenu.classList.contains("open-menu")) {
+            searchDDMenu.style.height=total+"px"
+        }
+        else{
+            searchDDMenu.style.height="0px"
+        }
+        
     })
-    if (searchDDMenu.classList.contains("open-menu")) {
-        searchDDMenu.style.height=total+"px"
-    }
-    else{
-        searchDDMenu.style.height="0px"
-    }
-    
 })
+
 
 
 
